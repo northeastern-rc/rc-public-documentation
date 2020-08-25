388778df5b4a38124a906a07213d046669b9cef5
Cho <ju.cho@northeastern.edu>
2020-08-24 22:34:02 -0400

    adding custom css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,9 +60,7 @@ html_static_path = ['_static']
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
-html_css_files = [
-    'css/custom.css',
-]
+html_css_files = ['custom.css']
 
 #If true, “(C) Copyright …” is shown in the HTML footer. Default is True.
 html_show_copyright = True
