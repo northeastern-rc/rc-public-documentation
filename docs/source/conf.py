a721892bd5fd35ed8ae02ffe019a082303649d20
Joseph P. Robinson <j.robinson@northeastern.edu>
2023-06-21 13:10:58 -0400

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,7 +62,9 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["first_steps/cps_ood.md", "using-discovery/bash.md", "_snippets/*"]
+exclude_patterns = ["first_steps/cps_ood.md",
+                    "using-discovery/bash.md",
+                    "_snippets/*"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -93,7 +95,8 @@ html_show_sphinx = False
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static", "_static/video"]
+html_static_path = ["_static",
+                    "_static/video"]
 
 master_doc = "index"
 source_suffix = [".rst", ".md"]
